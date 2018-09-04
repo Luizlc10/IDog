13c0095f6f832ba18c68bbc44a94ebf18958748d
Luiz Claudio Lopes Rocha <luiz.claudio@academico.ifrn.edu.br>
2018-09-04 19:11:16 -0300

    feature/feed: tests

---
+++ src/pages/feed/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom'
+
+import './feed.css'
+
+
+export default class Feed extends Component {
+  render() {
+    return (
+      <div className="login-page">
+        <h1>Feed</h1>
+      </div>
+    )
+  }
+}
