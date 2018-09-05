2ceaa945732576e140082f988a8402f50f821a59
Luiz Claudio Lopes Rocha <luiz.claudio@academico.ifrn.edu.br>
2018-09-05 02:32:02 -0300

    template/galery cell: creating test

---
+++ src/components/cell/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class Cell extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="cell">
+        <h1 >cell</h1>
+      </div>
+    )
+  }
+}
