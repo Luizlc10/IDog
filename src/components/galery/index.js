2f22ec50ff1a5fa4a5355bc4e57a0ae9e4905804
Luiz Claudio Lopes Rocha <luiz.claudio@academico.ifrn.edu.br>
2018-09-05 00:16:43 -0300

    galery: rendering data by props

--- src/components/galery/index.js
+++ src/components/galery/index.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 
 export default class Menu extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <div className="galery">
+        <ul>
+          {this.props.list.map((item, i) => {
+            return(
+              <li key={i}>{item}</li>
+            )
+          })}
+        </ul>
       </div>
     )
   }
