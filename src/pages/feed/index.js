f3876d80f35d41cb21e2a92de5151dce1a9f9340
Luiz Claudio Lopes Rocha <luiz.claudio@academico.ifrn.edu.br>
2018-09-04 22:01:52 -0300

    feature/feed: getting data

--- src/pages/feed/index.js
+++ src/pages/feed/index.js
@@ -3,12 +3,50 @@ import { BrowserRouter, Route, Switch, Link } from 'react-router-dom'
 
 import './feed.css'
 
-
+import api from '../../services/api';
 export default class Feed extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentBeed: '',
+      isLoading: false
+    }
+  }
+
+  componentDidMount() {
+    this.getDogs();
+  }
+
+  getDogs = async () => {
+    try {
+      this.setState({
+        isLoading: true,
+        errorText: ''
+      });
+
+      const token = await localStorage.getItem('token');
+      const req = await api.get('/feed', { headers: {
+          Authorization: token
+        }
+      });
+
+      this.setState({
+        data: req.data,
+        isLoading: false
+      });
+
+    } catch (error) {
+      this.setState({
+        errorText: error.response.data.error.message,
+        isLoading: false,
+      });
+    }
+  }
+
   render() {
     return (
-      <div className="login-page">
-        <h1>Feed</h1>
+      <div className="feed-page">
+        <h1 className="feed-page-title">IDog</h1>
       </div>
     )
   }
